1bdb66a43644ddbcc80beb11072801207b5187b7
MaxTheFirst <maks.vershinin.2005@mail.ru>
2025-05-07 00:28:34 +0300

    add translator

--- src/main/java/com/example/demo/parser/BaseParser.java
+++ src/main/java/com/example/demo/parser/BaseParser.java
@@ -1,16 +1,19 @@
 package com.example.demo.parser;
 
 import com.example.demo.domain.dto.ArticleDTO;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-
 @Slf4j
 public abstract class BaseParser extends HttpParser implements SiteParser {
+
+  @Getter
+  protected final String language = "ru";
 
   public abstract String getNAME();
 
@@ -20,15 +23,16 @@ public abstract class BaseParser extends HttpParser implements SiteParser {
   @Override
   public abstract CompletableFuture<List<String>> getArticleLinks();
 
-    protected CompletableFuture<List<String>> getArticleLinks(String blogLink) {
-        return getPage(blogLink)
-                .thenApply(page -> {
-                    if (page.isEmpty()) {
-                        return List.of();
-                    }
+  protected CompletableFuture<List<String>> getArticleLinks(String blogLink) {
+    return getPage(blogLink)
+        .thenApply(page -> {
+          if (page.isEmpty()) {
+            return List.of();
+          }
                     List<String> articlesLinks = getArticleLinks(page.get());
-                    return articlesLinks.size() > limitArticleCount ? articlesLinks.subList(0, limitArticleCount) : articlesLinks;
-                });
+          return articlesLinks.size() > limitArticleCount ? articlesLinks.subList(0,
+              limitArticleCount) : articlesLinks;
+        });
   }
 
   protected abstract List<String> getArticleLinks(Document page);
--- src/main/java/com/example/demo/parser/sites/InfoqParser.java
+++ src/main/java/com/example/demo/parser/sites/InfoqParser.java
@@ -2,23 +2,24 @@ package com.example.demo.parser.sites;
 
 import com.example.demo.domain.dto.ArticleDTO;
 import com.example.demo.parser.BaseParser;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-
 @Component
 @Slf4j
 public class InfoqParser extends BaseParser {
   @Getter
   private final String NAME = "Infoq";
+  @Getter
+  private final String language = "en";
 
   private static final String DOMAIN = "https://www.infoq.com";
   private static final String BLOG_LINK = "https://www.infoq.com/development/";
@@ -63,11 +64,16 @@ public class InfoqParser extends BaseParser {
         }
       }
 
+      String date = "";
+      if (dateElement != null) {
+        date = dateElement.text().trim();
+      }
+
       return Optional.ofNullable(ArticleDTO.builder()
           .name(titleElement.text())
           .description(textBuilder.toString().trim())
           .url(link)
-          .date(dateElement.text())
+          .date(date)
           .build());
     } catch (Exception e) {
       log.error("Ошибка парсинга новости: {}", link, e);
